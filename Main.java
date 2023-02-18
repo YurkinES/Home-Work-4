dadd9cc2970547b4b901b9a03184253e95b33528
YurkinES <6eJlomop@inbox.ru>
2023-02-18 12:48:24 +0300

    Fifyh stage

--- Main.java
+++ Main.java
@@ -21,5 +21,31 @@ public class Main {
         } else {
             System.out.println("Если скорость " + carSpeed + " можно ездить спокойно");
         }
+    //Task #4
+        byte ageHum4 = 18;
+        if (ageHum4 >= 2 && ageHum4 <= 6) {
+            System.out.println("Если возраст человека " + ageHum4 + " то ему нужно идти в детский сад");
+        }
+        if (ageHum4 >= 7 && ageHum4 <= 18) {
+            System.out.println("Если возраст человека " + ageHum4 + " то ему нужно ходить в школу");
+        }
+        if (ageHum4 > 18 && ageHum4 <= 24) {
+            System.out.println("Если возраст человека " + ageHum4 + " то его место в университете");
+        }
+        if (ageHum4 > 24) {
+            System.out.println("Если возраст человека " + ageHum4 + " то ему пора ходить на работу");
+        }
+    //Task #5
+        byte ageHum5 = 16;
+        if (ageHum5 < 5) {
+            System.out.println("Если возраст человека " + ageHum5 + " то он не может кататься на атракционе");
+        }
+        if (ageHum5 >= 5 && ageHum5 < 14) {
+            System.out.println("Если возраст человека " + ageHum5 + " то он может кататься на атракционе только в сопровождении взрослого");
+        }
+        if (ageHum5 > 14) {
+            System.out.println("Если возраст человека " + ageHum5 + " то он может кататься на атракционе без сопровождения взрослого");
+        }
+        }
     }
-}
+
