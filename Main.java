370af3995496178104d7c72e2e4f0cada87928cb
YurkinES <6eJlomop@inbox.ru>
2023-02-18 14:07:27 +0300

    Final stage

--- Main.java
+++ Main.java
@@ -25,25 +25,20 @@ public class Main {
         byte ageHum4 = 18;
         if (ageHum4 >= 2 && ageHum4 <= 6) {
             System.out.println("Если возраст человека " + ageHum4 + " то ему нужно идти в детский сад");
-        }
-        if (ageHum4 >= 7 && ageHum4 <= 18) {
+        }else if (ageHum4 >= 7 && ageHum4 <= 18) {
             System.out.println("Если возраст человека " + ageHum4 + " то ему нужно ходить в школу");
-        }
-        if (ageHum4 > 18 && ageHum4 <= 24) {
+        }else if (ageHum4 > 18 && ageHum4 <= 24) {
             System.out.println("Если возраст человека " + ageHum4 + " то его место в университете");
-        }
-        if (ageHum4 > 24) {
+        }else if (ageHum4 > 24) {
             System.out.println("Если возраст человека " + ageHum4 + " то ему пора ходить на работу");
         }
     //Task #5
         byte ageHum5 = 16;
         if (ageHum5 < 5) {
             System.out.println("Если возраст человека " + ageHum5 + " то он не может кататься на атракционе");
-        }
-        if (ageHum5 >= 5 && ageHum5 < 14) {
+        }else if (ageHum5 >= 5 && ageHum5 < 14) {
             System.out.println("Если возраст человека " + ageHum5 + " то он может кататься на атракционе только в сопровождении взрослого");
-        }
-        if (ageHum5 > 14) {
+        }else if (ageHum5 > 14) {
             System.out.println("Если возраст человека " + ageHum5 + " то он может кататься на атракционе без сопровождения взрослого");
         }
     //Task #6
