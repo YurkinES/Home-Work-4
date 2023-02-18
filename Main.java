9bfbaf5ea3349707d52d1362c82f66983c8495b8
YurkinES <6eJlomop@inbox.ru>
2023-02-18 12:00:08 +0300

    Second stage

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //Task #1
+        byte ageHuman = 16;
+        if (ageHuman >= 18) {
+            System.out.println("Если возраст человека равен " + ageHuman + " то он соверщеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + ageHuman + " то он не соверщеннолетний, нужно немного подождать :(");
+        }
+    //Task #2
+        byte OutdoorTemp = 6;
+        if (OutdoorTemp >= 5) {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        } else {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
     }
 }
